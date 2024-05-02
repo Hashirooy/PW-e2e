1fb1ae208884134fcf298e516c282b02c1680b74
Hashirooy <battlenetruss@gmail.com>
2024-05-02 19:10:59 +0700

    change test name v2

--- tests/suites/api/posts/posts.spec.ts
+++ tests/suites/api/posts/posts.spec.ts
@@ -1,6 +1,6 @@
 import { test } from "../../../pageObjectFixtures/posts.fixture";
 
-test.only("get posts of list", async ({ postsApi }) => {
+test.only("get posts of list posts", async ({ postsApi }) => {
   await postsApi.getToken();
   const postsList = await postsApi.getListOfPosts();
   await postsApi.checkNumberofPosts(postsList.length, 16);
