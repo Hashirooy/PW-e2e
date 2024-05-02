eaa8381d128844f9eddc612e4b8e5b74682a80b1
Hashirooy <battlenetruss@gmail.com>
2024-05-02 18:18:30 +0700

    new api v3

--- tests/suites/api/posts/posts.spec.ts
+++ tests/suites/api/posts/posts.spec.ts
@@ -3,5 +3,5 @@ import { test } from "../../../pageObjectFixtures/posts.fixture";
 test.only("get posts list", async ({ postsApi }) => {
   await postsApi.getToken();
   const postsList = await postsApi.getListOfPosts();
-  //   await postsApi.checkNumberofPosts(postsList.length, 16);
+  await postsApi.checkNumberofPosts(postsList.length, 16);
 });
